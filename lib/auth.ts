3cd370246b39a238cb056089fe9dcdeeef008b8f
Darshankhnvr <darshankgoudahnvr@gmail.com>
2025-02-26 13:26:22 +0530

    middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -58,9 +58,15 @@ export const authOptions: NextAuthOptions = {
         return session
     }
   },
+  pages:{
+    signIn: "/login",
+    error: "/login"
+  },
   session:{
-    strategy: "jwt"
-  }
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60* 60
+  },
+  secret:process.env.NEXTAUTH_SECRET
 };
 
 
